ca09ac5ef116aaeb7c10ec708f079c6f52896b95
Viktor Charypar <charypar@gmail.com>
2014-02-25 16:43:00 +0000

    Basic operations for redis refdb backend

--- redis/hiredis.c
+++ redis/hiredis.c
@@ -28,6 +28,7 @@
 #include <git2.h>
 #include <git2/sys/odb_backend.h>
 #include <git2/sys/refdb_backend.h>
+#include <git2/sys/refs.h>
 #include <hiredis/hiredis.h>
 
 typedef struct {
@@ -37,19 +38,28 @@ typedef struct {
 	redisContext *db;
 } hiredis_odb_backend;
 
+typedef struct {
+	git_refdb_backend parent;
+
+	const char *repo_path;
+	redisContext *db;
+} hiredis_refdb_backend;
+
+/* Odb methods */
+
 int hiredis_odb_backend__read_header(size_t *len_p, git_otype *type_p, git_odb_backend *_backend, const git_oid *oid)
 {
 	hiredis_odb_backend *backend;
 	int error;
 	redisReply *reply;
-	char *str_id = calloc(41, sizeof(char));
+	char *str_id = calloc(GIT_OID_HEXSZ + 1, sizeof(char));
 
 	assert(len_p && type_p && _backend && oid);
 
 	backend = (hiredis_odb_backend *) _backend;
 	error = GIT_ERROR;
 
-	git_oid_tostr(str_id, 40, oid);
+	git_oid_tostr(str_id, GIT_OID_HEXSZ, oid);
 
 	reply = redisCommand(backend->db, "HMGET rugged:%s:odb:%s %s %s", backend->repo_path, str_id, "type", "size");
 	free(str_id);
@@ -76,14 +86,14 @@ int hiredis_odb_backend__read(void **data_p, size_t *len_p, git_otype *type_p, g
 	hiredis_odb_backend *backend;
 	int error;
 	redisReply *reply;
-	char *str_id = calloc(41, sizeof(char));
+	char *str_id = calloc(GIT_OID_HEXSZ + 1, sizeof(char));
 
 	assert(data_p && len_p && type_p && _backend && oid);
 
 	backend = (hiredis_odb_backend *) _backend;
 	error = GIT_ERROR;
 
-	git_oid_tostr(str_id, 40, oid);
+	git_oid_tostr(str_id, GIT_OID_HEXSZ, oid);
 
 	reply = redisCommand(backend->db, "HMGET rugged:%s:odb:%s %s %s %s", backend->repo_path, str_id,
 			"type", "size", "data");
@@ -124,10 +134,10 @@ int hiredis_odb_backend__read_prefix(git_oid *out_oid,
 			git_oid_cpy(out_oid, short_oid);
 
 		return error;
-	} else if (len < GIT_OID_HEXSZ) {
-		/* TODO */
-		return GITERR_INVALID;
 	}
+
+	/* TODO prefix */
+	return GITERR_INVALID;
 }
 
 int hiredis_odb_backend__exists(git_odb_backend *_backend, const git_oid *oid)
@@ -135,14 +145,14 @@ int hiredis_odb_backend__exists(git_odb_backend *_backend, const git_oid *oid)
 	hiredis_odb_backend *backend;
 	int found;
 	redisReply *reply;
-	char *str_id = calloc(41, sizeof(char));
+	char *str_id = calloc(GIT_OID_HEXSZ + 1, sizeof(char));
 
 	assert(_backend && oid);
 
 	backend = (hiredis_odb_backend *) _backend;
 	found = 0;
 
-	git_oid_tostr(str_id, 40, oid);
+	git_oid_tostr(str_id, GIT_OID_HEXSZ, oid);
 
 	reply = redisCommand(backend->db, "exists rugged:%s:odb:%s", backend->repo_path, str_id);
 	if (reply->type == REDIS_REPLY_INTEGER)
@@ -158,14 +168,14 @@ int hiredis_odb_backend__write(git_odb_backend *_backend, const git_oid *oid, co
 	hiredis_odb_backend *backend;
 	int error;
 	redisReply *reply;
-	char *str_id = calloc(41, sizeof(char));
+	char *str_id = calloc(GIT_OID_HEXSZ + 1, sizeof(char));
 
 	assert(oid && _backend && data);
 
 	backend = (hiredis_odb_backend *) _backend;
 	error = GIT_ERROR;
 
-	git_oid_tostr(str_id, 40, oid);
+	git_oid_tostr(str_id, GIT_OID_HEXSZ, oid);
 
 	reply = redisCommand(backend->db, "HMSET rugged:%s:odb:%s "
 			"type %d "
@@ -191,6 +201,204 @@ void hiredis_odb_backend__free(git_odb_backend *_backend)
 
 	free(backend);
 }
+
+/* Refdb methods */
+
+int hiredis_refdb_backend__exists(int *exists, git_refdb_backend *_backend, const char *ref_name)
+{
+	hiredis_refdb_backend *backend;
+	int error = GIT_OK;
+	redisReply *reply;
+
+	assert(ref_name && _backend);
+
+	backend = (hiredis_refdb_backend *) _backend;
+
+	reply = redisCommand(backend->db, "EXISTS rugged:%s:refdb:%s", backend->repo_path, ref_name);
+	if (reply->type == REDIS_REPLY_INTEGER)
+		*exists = reply->integer;
+	else
+		error = GIT_ERROR;
+
+	freeReplyObject(reply);
+	return error;
+}
+
+int hiredis_refdb_backend__lookup(git_reference **out, git_refdb_backend *_backend, const char *ref_name)
+{
+	hiredis_refdb_backend *backend;
+	int error = GIT_OK;
+	redisReply *reply;
+	git_oid oid;
+
+	assert(ref_name && _backend);
+
+	backend = (hiredis_refdb_backend *) _backend;
+
+	reply = redisCommand(backend->db, "HMGET rugged:%s:refdb:%s type target", backend->repo_path, ref_name);
+	if(reply->type == REDIS_REPLY_ARRAY) {
+		if (reply->element[0]->type != REDIS_REPLY_NIL && reply->element[1]->type != REDIS_REPLY_NIL) {
+			git_ref_t type = (git_ref_t) atoi(reply->element[0]->str);
+
+			if (type == GIT_REF_OID) {
+				printf("[REDIS] Found (oid) %s -> %s\n", ref_name, reply->element[1]->str);
+				git_oid_fromstr(&oid, reply->element[1]->str);
+				*out = git_reference__alloc(ref_name, &oid, NULL);
+			} else if (type == GIT_REF_SYMBOLIC) {
+				printf("[REDIS] Found (symbolic) %s -> %s\n", ref_name, reply->element[1]->str);
+				*out = git_reference__alloc_symbolic(ref_name, reply->element[1]->str);
+			} else {
+				error = GIT_ERROR;
+			}
+
+		} else {
+			error = GIT_ENOTFOUND;
+		}
+	} else {
+		error = GIT_ERROR;
+	}
+
+	freeReplyObject(reply);
+	return error;
+}
+
+int hiredis_refdb_backend__iterator(git_reference_iterator **iter, struct git_refdb_backend *_backend, const char *glob)
+{
+	printf("[REDIS] Iterate through references %s\n", glob);
+
+	return GIT_ERROR;
+}
+
+int hiredis_refdb_backend__write(git_refdb_backend *_backend, const git_reference *ref, int force, const git_signature *who,
+	const char *message) // FIXME when rugged gets updated to the latest libgit2 update this to the latest backend API
+{
+	hiredis_refdb_backend *backend;
+	int error = GIT_OK;
+	redisReply *reply;
+
+	const char *name = git_reference_name(ref);
+	const git_oid *target;
+	const char *symbolic_target;
+	char oid_str[GIT_OID_HEXSZ + 1];
+
+	assert(ref && _backend);
+
+	backend = (hiredis_refdb_backend *) _backend;
+
+	target = git_reference_target(ref);
+	symbolic_target = git_reference_symbolic_target(ref);
+
+	/* FIXME handle force */
+
+	if (target) {
+		git_oid_nfmt(oid_str, sizeof(oid_str), target);
+		reply = redisCommand(backend->db, "HMSET rugged:%s:refdb:%s type %d target %s", backend->repo_path, name, GIT_REF_OID, oid_str);
+	} else {
+		symbolic_target = git_reference_symbolic_target(ref);
+		reply = redisCommand(backend->db, "HMSET rugged:%s:refdb:%s type %d target %s", backend->repo_path, name, GIT_REF_SYMBOLIC, symbolic_target);
+	}
+
+	if(reply->type == REDIS_REPLY_ERROR)
+		error = GIT_ERROR;
+
+	freeReplyObject(reply);
+	return error;
+}
+
+int hiredis_refdb_backend__rename(git_reference **out, git_refdb_backend *_backend, const char *old_name,
+	const char *new_name, int force, const git_signature *who, const char *message)
+{
+	hiredis_refdb_backend *backend;
+	int error = GIT_OK;
+	redisReply *reply;
+
+	assert(old_name && new_name && _backend);
+
+	backend = (hiredis_refdb_backend *) _backend;
+
+	printf("[REDIS] Rename reference %s -> %s\n", old_name, new_name);
+
+	reply = redisCommand(backend->db, "RENAME rugged:%s:refdb:%s rugged:%s:refdb:%s",
+						backend->repo_path, old_name, backend->repo_path, new_name);
+	if(reply->type == REDIS_REPLY_ERROR) {
+		freeReplyObject(reply);
+		return GIT_ERROR;
+	}
+
+	freeReplyObject(reply);
+	return hiredis_refdb_backend__lookup(out, _backend, new_name);
+}
+
+int hiredis_refdb_backend__del(git_refdb_backend *_backend, const char *ref_name) // FIXME when rugged gets updated to the latest libgit2 update this to the latest backend API
+{
+	hiredis_refdb_backend *backend;
+	int error = GIT_OK;
+	redisReply *reply;
+
+	assert(ref_name && _backend);
+
+	backend = (hiredis_refdb_backend *) _backend;
+
+	printf("[REDIS] Delete reference %s\n", ref_name);
+
+	reply = redisCommand(backend->db, "DEL rugged:%s:refdb:%s", backend->repo_path, ref_name);
+	if(reply->type == REDIS_REPLY_ERROR)
+		error = GIT_ERROR;
+
+	freeReplyObject(reply);
+	return error;
+}
+
+/* reflog methods */
+
+int hiredis_refdb_backend__has_log(git_refdb_backend *_backend, const char *refname)
+{
+	printf("[REDIS] Reference has log? %s\n", refname);
+
+	return GIT_ERROR;
+}
+
+int hiredis_refdb_backend__ensure_log(git_refdb_backend *_backend, const char *refname)
+{
+	printf("[REDIS] Reference ensure log %s\n", refname);
+
+	return GIT_ERROR;
+}
+
+void hiredis_refdb_backend__free(git_refdb_backend *_backend)
+{
+	printf("[REDIS] Free ref backend log\n");
+}
+
+int hiredis_refdb_backend__reflog_read(git_reflog **out, git_refdb_backend *_backend, const char *name)
+{
+	printf("[REDIS] Reference read log %s\n", name);
+
+	return GIT_ERROR;
+}
+
+int hiredis_refdb_backend__reflog_write(git_refdb_backend *_backend, git_reflog *reflog)
+{
+	printf("[REDIS] Reference write log\n");
+
+	return GIT_ERROR;
+}
+
+int hiredis_refdb_backend__reflog_rename(git_refdb_backend *_backend, const char *old_name, const char *new_name)
+{
+	printf("[REDIS] Reference rename log %s -> %s\n", old_name, new_name);
+
+	return GIT_ERROR;
+}
+
+int hiredis_refdb_backend__reflog_delete(git_refdb_backend *_backend, const char *name)
+{
+	printf("[REDIS] Reference delete log %s\n", name);
+
+	return GIT_ERROR;
+}
+
+/* Constructors */
 
 int git_odb_backend_hiredis(git_odb_backend **backend_out, const char* path, const char *host, int port)
 {
@@ -224,3 +432,41 @@ int git_odb_backend_hiredis(git_odb_backend **backend_out, const char* path, con
 
 	return GIT_OK;
 }
+
+int git_refdb_backend_hiredis(git_refdb_backend **backend_out, const char* path, const char *host, int port)
+{
+	hiredis_refdb_backend *backend;
+
+	backend = calloc(1, sizeof(hiredis_refdb_backend));
+	if (backend == NULL)
+		return GITERR_NOMEMORY;
+
+	backend-> db = redisConnect(host, port);
+	if (backend->db->err) {
+		free(backend);
+		return GIT_ERROR;
+	}
+
+	backend->repo_path = path;
+
+	backend->parent.exists = &hiredis_refdb_backend__exists;
+	backend->parent.lookup = &hiredis_refdb_backend__lookup;
+	backend->parent.iterator = &hiredis_refdb_backend__iterator;
+	backend->parent.write = &hiredis_refdb_backend__write;
+	backend->parent.del = &hiredis_refdb_backend__del;
+	backend->parent.rename = &hiredis_refdb_backend__rename;
+	backend->parent.compress = NULL;
+	backend->parent.free = &hiredis_refdb_backend__free;
+
+	backend->parent.has_log = &hiredis_refdb_backend__has_log;
+	backend->parent.ensure_log = &hiredis_refdb_backend__ensure_log;
+	backend->parent.reflog_read = &hiredis_refdb_backend__reflog_read;
+	backend->parent.reflog_write = &hiredis_refdb_backend__reflog_write;
+	backend->parent.reflog_rename = &hiredis_refdb_backend__reflog_rename;
+	backend->parent.reflog_delete = &hiredis_refdb_backend__reflog_delete;
+
+	*backend_out = (git_refdb_backend *) backend;
+
+	return GIT_OK;
+}
+
